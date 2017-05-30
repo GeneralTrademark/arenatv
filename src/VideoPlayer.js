294a88eaba76f4586fd98ca8d5c629c97240a828
Callil Capuozzo <callil>
2017-05-30 17:13:38 -0400

    Update from master

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import ReactDOM from 'react-dom'
-import YouTube from './YouTube'
+import YouTube from 'react-youtube'
 
 class VideoPlayer extends React.Component {
   constructor(props) {
