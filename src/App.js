ddbe31e83ee052ff01fcf2efa271532cf74ff6d7
Callil Capuozzo <callil>
2017-05-30 16:23:29 -0400

    Update youtube behavior

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './VideoPlayer'
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import VideoPlayer from './VideoPlayer'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h2>Welcome to arenatv</h2>
         </div>
-        <VideoPlayer />
+        <VideoPlayer seekTime={10} videoUrl="iYJKd0rkKss" />
       </div>
     )
   }
--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,56 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import YouTube from '../';
-
-const videoIdA = 'XxVg_s8xAms';
-const videoIdB = '-DX3vJiqxm4';
+import React from 'react'
+// import ReactDOM from 'react-dom'
+import YouTube from './YouTube'
 
 class VideoPlayer extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      videoId: videoIdA,
+      videoId: props.videoUrl,
       player: null,
-    };
-
-    this.onReady = this.onReady.bind(this);
-    this.onChangeVideo = this.onChangeVideo.bind(this);
-    this.onPlayVideo = this.onPlayVideo.bind(this);
-    this.onPauseVideo = this.onPauseVideo.bind(this);
+      muted: false,
+    }
   }
 
-  onReady(event) {
-    console.log(`YouTube Player object for videoId: "${this.state.videoId}" has been saved to state.`); // eslint-disable-line
+  onReady = (event) => {
+    console.log(`YouTube Player object for videoId: "${this.state.videoId}" has been saved to state.`) // eslint-disable-line
     this.setState({
       player: event.target,
-    });
+    })
+    this.state.player.playVideo()
+    this.state.player.removeEventListener('click', 'pause')
+    // console.log(this.state.player.getDuration())
+    this.state.player.seekTo(this.props.seekTime, true)
   }
 
-  onPlayVideo() {
-    this.state.player.playVideo();
-  }
-
-  onPauseVideo() {
-    this.state.player.pauseVideo();
-  }
-
-  onChangeVideo() {
+  onMuteVideo = () => {
+    this.state.muted ? this.state.player.unMute() : this.state.player.mute()
     this.setState({
-      videoId: this.state.videoId === videoIdA ? videoIdB : videoIdA,
-    });
+      muted: !this.state.muted,
+    })
+  }
+
+  preventPause = (event) => {
+    console.log('hi')
+    event.preventDefault()
   }
 
   render() {
+    const opts = {
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        theme: 'dark',
+        fs: 0,
+        rel: 0,
+        controls: 0,
+        modestbranding: 1,
+        autohide: 1,
+        showinfo: 0,
+      },
+    }
+
     return (
       <div>
-        <YouTube videoId={this.state.videoId} onReady={this.onReady} />
-        <button onClick={this.onPlayVideo}>Play</button>
-        <button onClick={this.onPauseVideo}>Pause</button>
-        <button onClick={this.onChangeVideo}>Change Video</button>
+        <YouTube
+          videoId={this.state.videoId}
+          onReady={this.onReady}
+          onClick={this.preventPause}
+          opts={opts}
+        />
+        <button onClick={this.onMuteVideo}>Mute</button>
+        {/* <button onClick={this.onPlayVideo}>Play</button> */}
+        {/* <button onClick={this.onPauseVideo}>Pause</button> */}
+        {/* <button onClick={this.onChangeVideo}>Change Video</button> */}
+        {/* <button onClick={this.onChangeTime}>time</button> */}
       </div>
-    );
+    )
   }
 }
 
-export default VideoPlayer;
+export default VideoPlayer
