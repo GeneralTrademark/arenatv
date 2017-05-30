65fc0a90fc3b03628508d32978ecec0b0373b3d3
Callil Capuozzo <callil>
2017-05-30 13:56:23 -0400

    Add Video player with react-youtube
    Won’t start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './VideoPlayer'
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h2>Welcome to arenatv</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <VideoPlayer />
       </div>
     );
   }
