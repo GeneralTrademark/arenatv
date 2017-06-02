430487b280129a9c08acca872f525ce85affdbfc
Gavin Atkinson <gatkinso@risd.edu>
2017-06-02 16:49:15 -0400

    Add arena methods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import base from './helpers/base'
 import './App.css'
 import Client from './Client'
 import ChannelList from './ChannelList'
@@ -16,21 +17,64 @@ class App extends Component {
   }
 
   componentWillMount = () => {
+    this.updateChannelList()
+    // this.updateVideos()
+  }
+
+  updateVideos = (toUpdate) => {
+    base.fetch('channels', {
+      context: this,
+      asArray: true,
+      then() {
+        Promise.all(toUpdate.map(channel =>
+          fetch(`${config.apiBase}/channels/${channel.slug}/contents`).then(resp => resp.json())
+        )).then((videos) => {
+          console.log(videos)
+        }).catch((error) => {
+          console.log('parsing failed', error)
+        })
+      },
+    })
+  }
+
+
+  // updateVideos = () => {
+  //   base.listenTo('votes', {
+  //   context: this,
+  //   asArray: true,
+  //   then(votesData){
+  //     var total = 0;
+  //     votesData.forEach((vote, index) => {
+  //       total += vote
+  //     });
+  //     this.setState({total});
+  //   }
+  // })
+  //   Promise.all(channels.map(block =>
+  //     fetch(`${config.apiBase}/channels/${block.slug}/contents`).then(resp => resp.json())
+  //       )).then(texts => {
+  //         console.log(texts)
+  //       }).catch(function (ex) {
+  //         console.log('parsing failed', ex)
+  //       })
+  // }
+
+
+  updateChannelList = () => {
     const component = this
     fetch(`${config.apiBase}/channels/${this.state.playlistChannelSlug}`)
       .then(function (response) {
         return response.json()
       }).then(function (response) {
         const channels = response.contents
-        console.log(channels)
         component.setState({ channels })
-        Promise.all(channels.map(block =>
-          fetch(`${config.apiBase}/channels/${block.slug}/contents`).then(resp => resp.json())
-            )).then(texts => {
-              console.log(texts)
-            })
-      }).catch(function (ex) {
-        console.log('parsing failed', ex)
+        Promise.all(channels.map(channel =>
+          base.post(channel.slug, {
+            data: { channel },
+          }).catch((err) => {
+            console.log(`Error post channels to FB: ${err}`)
+          })
+        ))
       })
   }
 
